d0f86c499e41bec05ea38f590c3c9f9916817d74
Srihari1798 <120098197+Srihari1798@users.noreply.github.com>
2023-05-20 20:39:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import CoinGecko from './CoinGecko';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+      );
+      const actualData = await res.json();
+      console.log(actualData);
+      setData(actualData);
+    } catch (error) {
+      console.log('Error fetching data:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className="table-responsive">
+      <table className="table  table-success table-striped-columns">
+        <thead className="table-dark">
+    
+       {/* <thead className='thead'> */}
+          <tr>
+            <th>NAME</th>
+            <th>ID</th>
+            <th>IMAGE</th>
+            <th>SYMBOL</th>
+            <th>CURRENT_PRICE</th>
+            <th>TOTAL_VOLUME</th>
+          </tr>
+        </thead>
+        <tbody className='tbody'>
+         
+          {data.map(item => (
+            <CoinGecko key={item.id} data={item} />
+            
+          ))}
+        
+         
+        </tbody>
+      </table>
     </div>
   );
 }
